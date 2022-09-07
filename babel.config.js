bc1ef12f8518c9a5bdf68d8f5aae5b27b50b0c20
Gabi Macovei (PERO-SP) <gabriel.macovei@porsche-engineering.ro>
2022-09-07 15:26:59 +0300

    Add uikitten

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
-};
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          cwd: 'babelrc',
+          extensions: ['.ts', '.tsx'],
+          alias: {
+            '@reddit': './src/@reddit'
+          }
+        }
+      ],
+    ]
+  };
